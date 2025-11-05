0d5cc758ce9c80a4bf2c1d3de3f3790f6b313b4c
Rajarshi49 <paulrajarshi66@gmail.com>
2025-11-05 12:12:42 +0000

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load model
-model = joblib.load(path + "random_forest_model.pkl")
+model = joblib.load("random_forest_model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
